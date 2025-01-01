7eae2a581fd3c521599c53c37df03cb3bf602a4a
aganyo360 <wilsonaganyo@gmail.com>
2025-01-01 20:12:37 +0300

    conditional update

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-let userName = String(prompt("Who's there?"))
+// let userName = String(prompt("Who's there?"))
 
-if (userName === "Admin"){
-    if(String(prompt("Password?")) == "TheMaster"){
-        alert("Welcome!")
-    } 
-    else if (prompt("Password?") === null){
-        alert("Canceled")
-    } else {
-        alert("Wrong password")
+// if (userName === "Admin"){
+//     if(String(prompt("Password?")) == "TheMaster"){
+//         alert("Welcome!")
+//     } 
+//     else if (prompt("Password?") === null){
+//         alert("Canceled")
+//     } else {
+//         alert("Wrong password")
+//     }
+// }
+// else if (userName === "" || userName === null){
+//     alert("Canceled")
+// }
+// else {
+//     alert("I don't know you")
+// }
+
+let username = +prompt("Who is this")
+
+if(username === "Master"){
+    let pass = +prompt("Enter Password")
+    if(pass === "Tusker01")
+        alert("Welcome, Successful Login")
+
+    else if( pass ==="" || pass === null){
+        alert("Cancelled")
     }
-}
-else if (userName === "" || userName === null){
-    alert("Canceled")
-}
-else {
-    alert("I don't know you")
-}
+    else{
+        alert("wrong password")
+    }}
+
+else{
+    alert("User doesn't exist")
+    }

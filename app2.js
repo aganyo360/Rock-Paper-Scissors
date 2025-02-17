1a04ae5761ce187204a7655be04e159a9f19a4a4
Brice Wilson <Brice.Wilson@avodemia.com>
2025-02-17 15:29:11 +0300

    add user interface for the game for easier interractivity

--- app2.js
+++ app2.js
@@ -1,48 +1,112 @@
-    //query and select all the button choices
-    let rock = document.querySelector('.rock')
-    let paper = document.querySelector('.paper')
-    let scissors = document.querySelector('.scissors')
+//query and select all the button choices
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissors = document.querySelector(".scissors");
+let displayResults = document.querySelector(".display-results");
+let userResult = document.querySelector(".userChoice");
+let userScore= 0;
+let computerScore =0
+let roundPlayed =0;
 
-    // lets get computer choice
-    let getComputerChoice = ()=>{
-        let x = Math.random()*3
-        // console.log(x) for the purposing of testng whether x is retuning anything
-        
-        if(x<1){
-            return `ROCK`
-        }
-        else if(x>=1 && x<2){
-            return `PAPER`
-        }
-        else{
-            return `SCISSORS`
-        }   
-        
+// lets get computer choice
+let getComputerChoice = () => {
+  let x = Math.random() * 3;
+  // console.log(x) for the purposing of testng whether x is retuning anything
+
+  if (x < 1) {
+    return `ROCK`;
+  } else if (x >= 1 && x < 2) {
+    return `PAPER`;
+  } else {
+    return `SCISSORS`;
+  }
+};
+
+let handleClick = (userChoice) => {
+  let computerChose = getComputerChoice();
+  let humanPick = userChoice.toUpperCase();
+  userResult.textContent = `User chose: ${humanPick} | Computer chose: ${computerChose}`;
+
+  // lets compare the values;
+    let result = compareChoice(humanPick,computerChose)
+    showResults(result)
+
+
+};
+
+let compareChoice= (userChoice, computerChoice)=>{
+    if(userChoice === computerChoice){
+        return `It's a tie`
     }
-    let handleClick = (userChoice)=>{
-        let userValue =userChoice.toUpperCase();
-        console.log("user selected:", userValue);
-        return userValue
-    };
-    rock.addEventListener('click', ()=>{
-        let computerSelection = getComputerChoice()
-        handleClick("ROCK")
-        console.log(computerSelection)
-       
+    if (
+        (userChoice ==="ROCK" && computerChoice ==="SCISSORS" ) ||
+        (userChoice==="PAPER" && computerChoice === "ROCK")||
+        (userChoice==="SCISSORS" && computerChoice === "PAPER")
+       ){
+        userScore++
+        return `You Won`
+       }
+    computerScore++   
+    return `computer wins`
+
+}
+rock.addEventListener("click", () => {
+  if(roundPlayed<5)handleClick("ROCK");
+});
+paper.addEventListener("click", () => {
+  if (roundPlayed<5) handleClick("PAPER");
+});
+scissors.addEventListener("click", () => {
+  if(roundPlayed<5)handleClick("SCISSORS");
+});
+
+let showResults  =(result) =>{
+    displayResults.innerHTML =''
+
+    let resultDisplay= document.createElement("p")
+    resultDisplay.textContent = result;
+    displayResults.appendChild(resultDisplay)
+
+    //let now show the current score for each round
+
+    let scoreDisplay = document.createElement("p")
+    scoreDisplay.textContent = `Score  - User:${userScore} Computer : ${computerScore    }`
+    displayResults.appendChild(scoreDisplay)
+
+    roundPlayed++
+
+    if(roundPlayed===5){
+        showFinalWinner();
     }
-    )
-    paper.addEventListener('click', ()=>{
-        handleClick("PAPER")                                                            
-    })
-    scissors.addEventListener('click', ()=>{
-        handleClick("SCISSORS") 
-    })
-
-
-// // Now let's dive in for some fun
-
-let playRound =()=>{
 
 }
 
+let showFinalWinner= ()=>{
+    let finalResult ='';
+    if(userScore > computerScore){
+        finalResult = `YOU WON THE GAME By ${userScore} Points`
+    }
+    else if(userScore<computerScore){
+        finalResult ="Computer Wins"
+    }
+    else{
+        finalResult =`It's a tie`
+    }
 
+
+    //Display the final results
+
+    displayResults.innerHTML =''
+    let finalResultDisplay = document.createElement("p")
+    finalResultDisplay.textContent = finalResult
+    displayResults.appendChild(finalResultDisplay)
+
+    let restartMessage = document.createElement("p");
+    restartMessage.textContent = "Click any button to play again!";
+    displayResults.appendChild(restartMessage);
+
+
+    userScore=0
+    computerScore=0;
+    roundPlayed=0
+}

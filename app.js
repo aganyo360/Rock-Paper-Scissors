4ca4015739cb3280cb175c43d47e8a73c5053ab0
Brice Wilson <Brice.Wilson@avodemia.com>
2025-02-17 15:49:58 +0300

    restore

--- app.js
+++ app.js
@@ -1,114 +1,115 @@
-// get computer input
+// // get computer input
 
-let getComputerChoice = ()=>{
-    let randomPick = Math.random()*3;
-    if(randomPick<1){
-        return "rock".toUpperCase()
-    }
-    else if(randomPick >1 && randomPick< 2){
-        return "paper".toUpperCase()
-    }
-    else if(randomPick>= 2){
-        return "scissors".toUpperCase()
-    }
-}
-// get human choice
+// let getComputerChoice = ()=>{
+//     let randomPick = Math.random()*3;
+//     if(randomPick<1){
+//         return "rock".toUpperCase()
+//     }
+//     else if(randomPick >1 && randomPick< 2){
+//         return "paper".toUpperCase()
+//     }
+//     else if(randomPick>= 2){
+//         return "scissors".toUpperCase()
+//     }
+// }
+// // get human choice
 
-let getHumanChoice=()=>{
-    let humanPick= prompt("Choose a selection: ")
+// let getHumanChoice=()=>{
+//     let humanPick= prompt("Choose a selection: ")
     
-    if(humanPick===null || humanPick.trim()==="" ){
-        alert("You didn't make a choice");
-        return null
-    }
-    humanPick = humanPick.toUpperCase()
-   if(humanPick === "ROCK" || humanPick==="PAPER" || humanPick=="SCISSORS"){
-        return humanPick;
-    }
+//     if(humanPick===null || humanPick.trim()==="" ){
+//         alert("You didn't make a choice");
+//         return null
+//     }
+//     humanPick = humanPick.toUpperCase()
+//    if(humanPick === "ROCK" || humanPick==="PAPER" || humanPick=="SCISSORS"){
+//         return humanPick;
+//     }
     
-    else{
-        alert("Invalid Choice Pick Rock or Scissors")
-        return null;
-    }
+//     else{
+//         alert("Invalid Choice Pick Rock or Scissors")
+//         return null;
+//     }
     
-};
-// getHumanChoice()
+// };
+// // getHumanChoice()
 
-let playRound = (humanChoice,computerChoice)=>{
+// let playRound = (humanChoice,computerChoice)=>{
   
 
-   // lets now set the logic to who will win the round
+//    // lets now set the logic to who will win the round
 
-   if(humanChoice===computerChoice){
-        console.log(`it's draw ${humanChoice} and ${computerChoice} are equal`)
-        return "draw"
-   }
+//    if(humanChoice===computerChoice){
+//         console.log(`it's draw ${humanChoice} and ${computerChoice} are equal`)
+//         return "draw"
+//    }
 
-   else if(
-    (humanChoice==="ROCK" && computerChoice==="SCISSORS") ||
-    (humanChoice==="PAPER" && computerChoice==="ROCK")||
-    (humanChoice==="SCISSORS" && computerChoice==="PAPER")
-   ){
-    console.log(`You win ${humanChoice} beats ${computerChoice}`)
-    return "human"
-   }
-   else{
-    console.log(`You lose ${computerChoice} beats ${humanChoice}`)
-    return "computer"
-   }
-
-
-
-}  
+//    else if(
+//     (humanChoice==="ROCK" && computerChoice==="SCISSORS") ||
+//     (humanChoice==="PAPER" && computerChoice==="ROCK")||
+//     (humanChoice==="SCISSORS" && computerChoice==="PAPER")
+//    ){
+//     console.log(`You win ${humanChoice} beats ${computerChoice}`)
+//     return "human"
+//    }
+//    else{
+//     console.log(`You lose ${computerChoice} beats ${humanChoice}`)
+//     return "computer"
+//    }
 
 
-let playGame =()=>{
-    let computerScore=0
-    let humanScore =0
-    for(let round= 1; round<6; round++){
 
-        let humanChoice=getHumanChoice()
-        let computerChoice=getComputerChoice()
+// }  
+
+
+// let playGame =()=>{
+//     let computerScore=0
+//     let humanScore =0
+//     for(let round= 1; round<6; round++){
+
+//         let humanChoice=getHumanChoice()
+//         let computerChoice=getComputerChoice()
         
-        if(humanChoice===null){
-            console.log("Invalid input. Skipping Round")
-            continue;
-        }
+//         if(humanChoice===null){
+//             console.log("Invalid input. Skipping Round")
+//             continue;
+//         }
         
-        console.log(`Round ${round} you chose ${humanChoice}, Computer chose ${computerChoice}`)
-        let scores =playRound(humanChoice, computerChoice)
+//         console.log(`Round ${round} you chose ${humanChoice}, Computer chose ${computerChoice}`)
+//         let scores =playRound(humanChoice, computerChoice)
         
-        if(scores==="human"){
-            humanScore++
-        }
-        else if(scores==="computer"){
-            computerScore++
-        }
+//         if(scores==="human"){
+//             humanScore++
+//         }
+//         else if(scores==="computer"){
+//             computerScore++
+//         }
         
-        console.log(`Current Score: YOU: ${humanScore} 
-            computer: ${computerScore}`)
+//         console.log(`Current Score: YOU: ${humanScore} 
+//             computer: ${computerScore}`)
         
-    }
-    if(humanScore> computerScore){
-        console.log(`Yown won by ${humanScore} Points`)
-    }
-    else if(humanScore==computerScore){
-        console.log("its a draw")
-    }
-    else{
-        console.log("computer wins by "+ computerScore)
-    }
-}
+//     }
+//     if(humanScore> computerScore){
+//         console.log(`Yown won by ${humanScore} Points`)
+//     }
+//     else if(humanScore==computerScore){
+//         console.log("its a draw")
+//     }
+//     else{
+//         console.log("computer wins by "+ computerScore)
+//     }
+// }
 
-let btnPlayGame = document.querySelector('#btn-playGame')
-btnPlayGame.addEventListener("click", ()=>{
-    playGame()
-})
+// let btnPlayGame = document.querySelector('#btn-playGame')
+// btnPlayGame.addEventListener("click", ()=>{
+//     playGame()
+// })
 
-let rockSelection = document.querySelector(".rock")
-let paperSelection = document.querySelector('.paper')
-let scissorsSelection = document.querySelector('.scissors')
-rockSelection=rockSelection.value
-paperSelection= paperSelection.value
-scissorsSelection = scissorsSelection.value
-console.log(rockSelection)
+// let rockSelection = document.querySelector(".rock")
+// let paperSelection = document.querySelector('.paper')
+// let scissorsSelection = document.querySelector('.scissors')
+// rockSelection=rockSelection.value
+// paperSelection= paperSelection.value
+// scissorsSelection = scissorsSelection.value
+// console.log(rockSelection)
+
---
+++ app2.js
@@ -0,0 +1,114 @@
+
+
+//query and select all the button choices
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissors = document.querySelector(".scissors");
+let displayResults = document.querySelector(".display-results");
+let userResult = document.querySelector(".userChoice");
+let userScore= 0;
+let computerScore =0
+let roundPlayed =0;
+
+// lets get computer choice
+let getComputerChoice = () => {
+  let x = Math.random() * 3;
+  // console.log(x) for the purposing of testng whether x is retuning anything
+
+  if (x < 1) {
+    return `ROCK`;
+  } else if (x >= 1 && x < 2) {
+    return `PAPER`;
+  } else {
+    return `SCISSORS`;
+  }
+};
+
+let handleClick = (userChoice) => {
+  let computerChose = getComputerChoice();
+  let humanPick = userChoice.toUpperCase();
+  userResult.textContent = `User chose: ${humanPick} | Computer chose: ${computerChose}`;
+
+  // lets compare the values;
+    let result = compareChoice(humanPick,computerChose)
+    showResults(result)
+
+
+};
+
+let compareChoice= (userChoice, computerChoice)=>{
+    if(userChoice === computerChoice){
+        return `It's a tie`
+    }
+    if (
+        (userChoice ==="ROCK" && computerChoice ==="SCISSORS" ) ||
+        (userChoice==="PAPER" && computerChoice === "ROCK")||
+        (userChoice==="SCISSORS" && computerChoice === "PAPER")
+       ){
+        userScore++
+        return `You Won`
+       }
+    computerScore++   
+    return `computer wins`
+
+}
+rock.addEventListener("click", () => {
+  if(roundPlayed<5)handleClick("ROCK");
+});
+paper.addEventListener("click", () => {
+  if (roundPlayed<5) handleClick("PAPER");
+});
+scissors.addEventListener("click", () => {
+  if(roundPlayed<5)handleClick("SCISSORS");
+});
+
+let showResults  =(result) =>{
+    displayResults.innerHTML =''
+
+    let resultDisplay= document.createElement("p")
+    resultDisplay.textContent = result;
+    displayResults.appendChild(resultDisplay)
+
+    //let now show the current score for each round
+
+    let scoreDisplay = document.createElement("p")
+    scoreDisplay.textContent = `Score  - User:${userScore} Computer : ${computerScore    }`
+    displayResults.appendChild(scoreDisplay)
+
+    roundPlayed++
+
+    if(roundPlayed===5){
+        showFinalWinner();
+    }
+
+}
+
+let showFinalWinner= ()=>{
+    let finalResult ='';
+    if(userScore > computerScore){
+        finalResult = `YOU WON THE GAME By ${userScore} Points`
+    }
+    else if(userScore<computerScore){
+        finalResult ="Computer Wins"
+    }
+    else{
+        finalResult =`It's a tie`
+    }
+
+
+    //Display the final results
+
+    displayResults.innerHTML =''
+    let finalResultDisplay = document.createElement("p")
+    finalResultDisplay.textContent = finalResult
+    displayResults.appendChild(finalResultDisplay)
+
+    let restartMessage = document.createElement("p");
+    restartMessage.textContent = "Click any button to play again!";
+    displayResults.appendChild(restartMessage);
+
+
+    userScore=0
+    computerScore=0;
+    roundPlayed=0
+}
